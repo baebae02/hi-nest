90594006b7bf0ee5b62d951ac9c6e81b05666ba7
Bae Seo Hyeon <bae4614@gmail.com>
2022-01-20 12:06:56 +0900

    feat: controller 생성

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+    @Get()
+    getAll(){
+        return 'This return all movies';
+    }
+    @Get('/:id')
+    getOne(@Param('id') id: string){
+        return `This will return one movie with the id : ${id}`;
+    }
+    @Post()
+    create(){
+        return 'This will create a movie';
+    }
+    @Delete('/:id')
+    remove(@Param('id') id: string){
+        return `This will delete a movie : ${id}`;
+    }
+    @Patch('/:id')
+    patch(@Param('id') id: string){
+        return `This will patch a movie : ${id}`;
+    }
+}
