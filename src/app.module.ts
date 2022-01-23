778526cd0512bfe4d140ff9037f1892d120f4be5
Bae Seo Hyeon <bae4614@gmail.com>
2022-01-24 00:26:58 +0900

    feat: seperate modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 //meta data
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController],
   //exports 4가지
 })
 export class AppModule {}
